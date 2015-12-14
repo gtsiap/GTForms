c250f9a9069ab5234da5ad4220807dec6f3a9740
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-14 12:51:17 +0200

    initial commit

---
+++ Source/GTForms.h
@@ -0,0 +1,18 @@
+//
+//  GTForms.h
+//  GTForms
+//
+//  Created by Giorgos Tsiapaliokas on 14/12/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTForms.
+FOUNDATION_EXPORT double GTFormsVersionNumber;
+
+//! Project version string for GTForms.
+FOUNDATION_EXPORT const unsigned char GTFormsVersionString[];
+
+
+
