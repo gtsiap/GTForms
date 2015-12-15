0006e5e8740594608ba66a9d67099f0025f3591c
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-15 15:20:08 +0200

    add FormSwitchView

--- Example/FormsVC.swift
+++ Example/FormsVC.swift
@@ -58,6 +58,7 @@ class FormsVC: FormTableViewController {
         createIntForms()
         createStringForms()
         createInfomationForms()
+        createMiscForms()
     }
 
     private func createDoubleForms() {
@@ -133,6 +134,17 @@ class FormsVC: FormTableViewController {
         
         self.formSections.append(section)
 
+    }
+    
+    private func createMiscForms() {
+        let title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        
+        let emailForm = FormSwitchView(title: title)
+        
+        let section = FormSection()
+        section.addRow(emailForm)
+        
+        self.formSections.append(section)
     }
     
     @objc private func didTapResultsButton() {
---
+++ Source/Views/FormSwitchView.swift
@@ -0,0 +1,97 @@
+// Copyright (c) 2015 Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+import SnapKit
+
+class Switcher: ControlLabelView  {
+    
+    private lazy private(set) var switcher: UISwitch = {
+        let switcher = UISwitch()
+        
+        switcher.addTarget(
+            self,
+            action: "switchValueDidChange",
+            forControlEvents: .ValueChanged
+        )
+        
+        return switcher
+    }()
+    
+    var valueDidChange: ((Bool) -> ())?
+    
+    override init() {
+        super.init()
+        
+        self.control = self.switcher
+        
+        configureView() { (label, control) in
+            
+            label.snp_makeConstraints() { make in
+                make.left.equalTo(self).offset(10)
+                make.top.equalTo(self).offset(10)
+              //  make.width.equalTo(self).multipliedBy(0.3)
+                make.bottom.equalTo(self).offset(-10)
+            } // end label
+            
+            
+            control.snp_makeConstraints() { make in
+                make.left.equalTo(label.snp_right)
+                make.right.equalTo(self).offset(-10)
+                make.centerY.equalTo(label.snp_centerY)
+            } // end control
+            
+        } // end configureView
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func switchValueDidChange() {
+        self.valueDidChange?(self.switcher.enabled)
+    }
+}
+
+
+public class FormSwitchView: BaseResultFormView<Bool> {
+
+    private let title: String
+    private let switcher = Switcher()
+
+    public init(title: String) {
+        self.title = title
+        super.init()
+        
+        self.switcher.controlLabel.text = title
+        
+        self.switcher.valueDidChange = { result in
+            self.updateResult(result)
+        }
+    }
+    
+    public override func validate() throws {}
+    
+    public override func formView() -> UIView {
+        return self.switcher
+    }
+    
+}
+
