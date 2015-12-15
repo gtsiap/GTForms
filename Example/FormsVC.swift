99e19941265b5d33202dda42f0badc9bc98d1dda
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-15 19:32:30 +0200

    fix constraints

--- Example/FormsVC.swift
+++ Example/FormsVC.swift
@@ -23,6 +23,8 @@ import GTForms
 
 class FormsVC: FormTableViewController {
    
+    private var longText: String = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    
     private lazy var resultsButton: UIBarButtonItem = {
         let button = UIBarButtonItem(
             title: "Results",
@@ -138,9 +140,7 @@ class FormsVC: FormTableViewController {
     }
     
     private func createMiscForms() {
-        let title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        
-        let emailForm = FormSwitchView(text: title)
+        let emailForm = FormSwitchView(text: self.longText)
         
         let section = FormSection()
         section.addRow(emailForm)
@@ -156,9 +156,17 @@ class FormsVC: FormTableViewController {
             ]
         )
         
+        let actionSheetPickerForm2 = FormActionSheetPickerView(
+            text: self.longText,
+            items: [
+                "1", "2", "3"
+            ]
+        )
+        
         let section = FormSection()
         section.addRow(actionSheetPickerForm)
-        
+        section.addRow(actionSheetPickerForm2)
+
         self.formSections.append(section)
     }
     
